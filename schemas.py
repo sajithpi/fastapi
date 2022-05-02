9287bc965cb9633e3c87a098d06232489fb3a218
Sajith P I <94563958+sajithpi@users.noreply.github.com>
2022-05-02 12:32:57 +0530

    added User Schemas

--- schemas.py
+++ schemas.py
@@ -14,3 +14,20 @@ class Post(BaseModel):
             title=title,
             description=description
         )
+class Users(BaseModel):
+    username:str
+    email:str
+    sponser_id:int
+    adrs : str
+    
+    @classmethod
+    def as_form(
+        cls,
+        username:str = Form(...),
+        email:str = Form(...),
+        sponser_id:int = Form(...),
+        adrs : str = Form(...)
+    ):
+        return cls(
+            username=username,email=email,sponser_id=sponser_id,adrs=adrs
+        )
